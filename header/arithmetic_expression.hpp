f1ab9f9bd729c7eeea913da2d584085c00e72537
mahdi-aouchiche <mahdiaouchiche2022@gmail.com>
2022-12-16 18:59:23 -0800

    Add header files

---
+++ header/arithmetic_expression.hpp
@@ -0,0 +1,114 @@
+#ifndef __ARITHMETIC_EXPRESSION_HPP__
+#define __ARITHMETIC_EXPRESSION_HPP__
+#include <iostream>
+#include <cstdlib>
+#include <stack> 
+#include <fstream> 
+#include <sstream>
+using namespace std;
+
+struct TreeNode
+{
+  char data;
+  char key;
+  TreeNode* left;
+  TreeNode* right;
+  TreeNode(char data, char key): data(data),key(key),left(nullptr),right(nullptr) { }
+};
+
+class arithmeticExpression
+{
+  private:
+    string infixExpression;
+    TreeNode* root;
+
+  public:
+    /* 
+      Initializes an empty tree and sets the infixExpression
+      to the value of parameter passed in. 
+    */
+    arithmeticExpression(const string &);
+
+    /*
+      Implementation of destrucor is optional.
+      The destructor should deallocate all the nodes in the tree.
+    */
+    ~arithmeticExpression();
+
+    /* 
+      Converts the infixExpression to its equivalent postfix string
+      and then generates the tree and assigns the root node to the 
+      root data field. The key for the first node is 'a', 'b'
+      for the second node and so on. 
+    */
+    void buildTree();
+
+    /* Calls the recursive infix function. */
+    void infix();
+
+    /* Calls the recursive prefix function. */
+    void prefix();
+
+    /* Calls the recursive postfix function. */
+    void postfix();
+
+    /*
+      Calls the recursive visualizeTree function and generates
+      the .png file using system call.
+    */
+    void visualizeTree(const string &);
+
+  private:
+    /*
+      Helper function that returns an integer according to
+      the priority of the given operator. 
+    */
+    int priority(char);
+
+    /* 
+      Helper function that returns the postfix notation equivalent
+      to the given infix expression 
+    */
+    string infix_to_postfix();
+
+    /* 
+      Helper function that outputs the infix notation
+      of the arithmetic expression tree
+      by performing the inorder traversal of the tree.
+      An opening and closing parenthesis must be added at the 
+      beginning and ending of each expression. 
+    */
+    void infix(TreeNode *);
+
+    /* 
+      Helper function that outputs the prefix notation
+      of the arithmetic expression tree
+      by performing the preorder traversal of the tree. 
+    */
+    void prefix(TreeNode *);
+
+    /* 
+      Helper function that outputs the postfix notation
+      of the arithmetic expression tree
+      by performing the postorder traversal of the tree. 
+    */
+    void postfix(TreeNode *);
+
+    /* 
+      Helper function for generating the dotty file.
+      This is a recursive function. 
+    */
+    void visualizeTree(ofstream &, TreeNode *);
+
+    /*
+      Helper function to set up nodes to write on the .dot file
+    */
+    void visualizeNodes(ofstream &, TreeNode *);
+
+    /*
+      Helper function to recursively delete tree nodes in the Destructor
+    */
+    void delete_node(TreeNode*); 
+};
+
+#endif  // __ARITHMETIC_EXPRESSION_HPP__
